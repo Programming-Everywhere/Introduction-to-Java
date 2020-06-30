963ce14867cc1891d9fa880c2a5b272c6ee02e97
XIAOLONGLII <1491skdpf89@gmail.com>
2020-06-29 22:09:56 -0400

    update file

--- Xiaolong/Chapter-1/PROGRAMMING_EXERCISES/onePointOne.java
+++ Xiaolong/Chapter-1/PROGRAMMING_EXERCISES/onePointOne.java
@@ -1,6 +1,6 @@
 class onePointOne {
   public static void main(String[] args) {  
-    System.out.println("Welcome to Jav); 
+    System.out.println("Welcome to Java); 
     System.out.println("Welcome to Computer Science");
     System.out.println("Programming is fun");
   }
