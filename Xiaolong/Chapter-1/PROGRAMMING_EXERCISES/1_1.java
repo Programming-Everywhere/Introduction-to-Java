fc7b069aa96c0c33efa8bfabe386dbf831545dcc
XIAOLONGLII <1491skdpf89@gmail.com>
2020-06-29 22:05:55 -0400

    added exercise

---
+++ Xiaolong/Chapter-1/PROGRAMMING_EXERCISES/1_1.java
@@ -0,0 +1,7 @@
+class 1.1 {
+  public static void main(String[] args) {  
+    System.out.println("Welcome to Jav); 
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is fun");
+  }
+}
