67df68974c3b7b15410b564f5998f5a19851db7b
XIAOLONGLII <1491skdpf89@gmail.com>
2020-07-01 18:18:43 -0400

    auto-update 2020-07-01-18:18:43

---
+++ Xiaolong/Chapter-1/PROGRAMMING_EXERCISES/OnePointTwo.java
@@ -0,0 +1,19 @@
+class OnePointTwo {
+   public static void main(String[] args) {
+     
+      for(int i = 0; i < 5; i++) {
+         System.out.println("Welcome to Java");
+      }
+   }
+}
+/*
+Result: 
+xiaolongl-macbookpro2% javac OnePointTwo.java&& java OnePointTwo      
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
+
+
+*/
