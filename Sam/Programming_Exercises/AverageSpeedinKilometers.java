a5f787182b19e0ba57b4109351b7eb46ec1ed5b4
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-07-03 15:44:18 -0500

    Update AverageSpeedinKilometers.java

--- Sam/Programming_Exercises/AverageSpeedinKilometers.java
+++ Sam/Programming_Exercises/AverageSpeedinKilometers.java
@@ -1,2 +1,37 @@
-""" Date: 07 02 2020 Author: Samantha Java Programming Exercises """ #Questions:
+  """ 
+  Date: 07 02 2020 
+  Author: Samantha 
+  Java Programming Exercises
+  """ 
+  #Questions:
+
+    
+
+#Answer
+
+
+public class Exercise_12 {
+}
+    public static void main(String[] strings) {
+
+        // Making variables to hold current time and distance
+        double hours = 1;
+        double minutes = 40;
+        double seconds = 35;
+        double distanceInMiles = 24;
+
+        // Converting from miles to kilometers
+        // Note: the book said 1.6, however 1.60934 is more accurate
+        double distanceInKilometers = distanceInMiles * 1.60934;
+
+        // Converting current time (hour, minutes, seconds) into total amount of minutes
+        double timeInMinutes = hours * 60.0 + minutes + seconds / 60.0;
+
+        // Calculating kilometers per hour
+        // kph = 60 * distance traveled / minutes taken
+        double kilometersPerHour = 60.0 * distanceInKilometers / timeInMinutes;
+
+        System.out.println(kilometersPerHour);
+
+    }
 
