5a89770e73fdaaa444dbe3b4c67c2a7a0da3b3ab
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-07-03 15:52:56 -0500

    Update and rename LinearEquations.md to LinearEquations.java

---
+++ Sam/Programming_Exercises/LinearEquations.java
@@ -0,0 +1,29 @@
+"""
+Date: 07 02 2020 
+Author: Samantha 
+Java Programming Exercises
+""" 
+#Questions:
+
+#Answer
+
+13
+
+public class Exercise_13 {
+}
+    public static void main(String[] args) {
+
+        // Variables for Cramer's formula
+        double a = 3.4;
+        double b = 50.2;
+        double c = 2.1;
+        double d = 0.55;
+        double e = 44.5;
+        double f = 5.9;
+
+        double x = (e * d - b * f) / (a * d - b * c);
+        double y = (a * f - e * c) / (a * d - b * c);
+
+        System.out.println("x = " + x + " y = " + y);
+
+    }
