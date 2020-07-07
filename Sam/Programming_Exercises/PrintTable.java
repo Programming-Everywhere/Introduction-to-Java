de76258455b91c71e7a3903612ca92869dac4285
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-07-07 16:38:01 -0500

    Update PrintTable.java

--- Sam/Programming_Exercises/PrintTable.java
+++ Sam/Programming_Exercises/PrintTable.java
@@ -1,18 +1,19 @@
-"""
+//
 Date: 07 02 2020
 Author: Samantha
 Java Programming Exercises
-"""
-#Questions:
+//
 
+
+//Questions:
 
 
 
 1.4 (Print a table) Write a program that displays the following table:
 
-public class Welcome{
+public class Welcome {
 
-     public static void main(String []args){
+    public static void main(String []args) {
         System.out.println("a   a^2   a^3");
         System.out.println("1    1    1");
         System.out.println("2    4    8");
@@ -20,5 +21,5 @@ public class Welcome{
         System.out.println("4    16   64");
        
         
-             }
+   }
 }
