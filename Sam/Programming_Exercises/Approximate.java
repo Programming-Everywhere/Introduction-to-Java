86c80a408c7e202c21445698c93da3584b2b4d0b
Samantha Anne <65324260+samanthaalcantara@users.noreply.github.com>
2020-07-07 16:29:24 -0500

    Update Approximate.java

--- Sam/Programming_Exercises/Approximate.java
+++ Sam/Programming_Exercises/Approximate.java
@@ -36,7 +36,7 @@ public class Welcome {
 
     public static void main(String []args){
         System.out.println(4 * 1.0 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
-        }
+    }    
 }
 
 //Output = 3.8209346209346213
