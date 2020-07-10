f6debe6f91ed94dc1a752d3b48c08cbdef2c4409
fuenshay <fuenshay@gmail.com>
2020-07-10 13:53:51 -0500

    2.10

---
+++ Shayna/Chapter-2/PROGRAMMING_EXERCISES/2.10.java
@@ -0,0 +1,38 @@
+/*Author: Shayna Fu
+Date: July 10, 2020
+
+
+2.10
+(Science: calculating energy) Write a program that calculates the energy
+needed to heat water from an initial temperature to a final temperature.
+Your program should prompt the user to enter the amount of water in
+kilograms and the initial and final temperatures of the water. The formula
+to compute the energy is
+
+Q = M * (finalTemperature - initialTemperature) * 4184
+
+where M is the weight of the water in kilograms, temperatures are in degrees
+Celsius, and energy is Q measure in joules.
+
+SOLUTION:*/
+
+import java.util.Scanner;
+
+class CalculatingEnergy {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the amount of water in kilograms: ");
+    double kilograms = input.nextDouble();
+
+    System.out.print("Enter the initial temperature: ");
+    double initialTemperature = input.nextDouble();
+
+    System.out.print("Enter the final temperature: ");
+    double finalTemperature = input.nextDouble();
+
+    double energy = kilograms * (finalTemperature - initialTemperature) * 4184;
+
+    System.out.println("The energy needed is " + energy);
+  }
+}
