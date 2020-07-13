2e9f0f54126eed0b6e3b0c9ede519123fc977e50
XIAOLONGLII <1491skdpf89@gmail.com>
2020-07-13 06:18:26 -0400

    exercises 1-10

---
+++ Xiaolong/Chapter-2/PROGRAMMING_EXERCISES/Two_10.java
@@ -0,0 +1,7 @@
+class Two_10 {
+  public static void main(String[] args) {
+     Scanner keyboard = new Scanner(System.in);
+     System.out.print("Enter a value for feet: ");
+     double feet = keyboard.nextDouble();
+  }
+}
